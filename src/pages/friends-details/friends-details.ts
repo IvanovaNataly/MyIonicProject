61c82cda1c3efb9f8541742f7d1fa4e2984366f2
NETCRAFT\nataliai <nataliai@netcraft.co.il>
2017-10-23 15:36:46 +0300

    clean up

--- src/pages/friends-details/friends-details.ts
+++ src/pages/friends-details/friends-details.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import FriendModel from '../../models/friendModel';
-import { FriendsPostsPage } from '../friends-posts/friends-posts';
 
 @Component({
     selector: 'page-friends-details',
@@ -9,17 +8,8 @@ import { FriendsPostsPage } from '../friends-posts/friends-posts';
 })
 export class FriendsDetailsPage {
     selectedFriend: FriendModel;
-    tab1: any;
-    // tab2: any;
 
     constructor(public navCtrl: NavController, public navParams: NavParams) {
         this.selectedFriend = this.navParams.get('item');
-        this.tab1 = FriendsPostsPage;
-        // this.tab2 = Tab2;
     }
-
-    // ionViewDidLoad() {
-    // console.log(this.selectedFriend);
-    // }
-
 }

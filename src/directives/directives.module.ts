6582d6d7f021c739c9c2aa752f47c087a79417da
NETCRAFT\nataliai <nataliai@netcraft.co.il>
2017-10-30 13:05:27 +0200

    lazyLoad directive created

--- src/directives/directives.module.ts
+++ src/directives/directives.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { OverslideDirective } from './overslide/overslide';
+import { LazyLoadDirective } from './lazy-load/lazy-load';
+
 @NgModule({
-	declarations: [OverslideDirective],
+	declarations: [OverslideDirective,
+    LazyLoadDirective],
 	imports: [],
-	exports: [OverslideDirective]
+	exports: [OverslideDirective,
+    LazyLoadDirective]
 })
 export class DirectivesModule {}
